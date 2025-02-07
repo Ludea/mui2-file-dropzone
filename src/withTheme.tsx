19e9373e228f7ba2c74cfe54f1837154e7907eac
Ludea <ludealol.olol@gmail.com>
2025-02-07 13:12:28 +0000

    fix theming

--- src/withTheme.tsx
+++ src/withTheme.tsx
@@ -4,17 +4,15 @@ import type { ComponentType } from "react";
 import React, { forwardRef } from "react";
 
 type WithoutTheme<P> = Omit<P, "theme">;
-
 function withTheme<P extends { theme?: Theme }>(Component: ComponentType<P>) {
-  return forwardRef(function ComponentWithTheme(
-    props: WithoutTheme<P>,
-    ref: any,
-  ) {
-    const theme = useTheme();
-    const combinedProps = { ...props, theme } as P;
+  return forwardRef<unknown, WithoutTheme<P>>(
+    function ComponentWithTheme(props, ref) {
+      const theme = useTheme();
+      const combinedProps = { ...props, theme } as unknown as P;
 
-    return <Component ref={ref} {...combinedProps} />;
-  });
+      return <Component ref={ref} {...combinedProps} />;
+    },
+  );
 }
 
 export { withTheme };
